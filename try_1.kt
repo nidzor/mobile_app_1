219bea32c45eed79800223887c75bb177117b39d
nidzor <160581694+nidzor@users.noreply.github.com>
2024-09-13 10:19:20 +0200

    Add files via upload

---
+++ try_1.kt
@@ -0,0 +1,15 @@
+package com.example.pro_1
+
+fun main() {
+//    var input = readln()
+//    val output = input.length
+//    println(output)
+
+//    var input = readln()
+//    if (input == "pomidor"){ println("pomidor") }
+//    else if (input == "truskawka"){ println("truskawka") }
+//    else { println( "To nie jest ani truskawka ani pomidor" )}
+
+
+
+}
